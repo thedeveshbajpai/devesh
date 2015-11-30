d70ba2626cd7be2846fdce7312acd124b8cad0e0
thedeveshbajpai <thedeveshbajpai@gmail.com>
2015-11-30 18:52:09 +0530

    more examples

--- https/github.com/thedeveshbajpai/Practise/src/Arraylist/ArrayListExample.java
+++ https/github.com/thedeveshbajpai/Practise/src/Arraylist/ArrayListExample.java
@@ -8,8 +8,8 @@ public class ArrayListExample {
 	  ArrayList<String> obj = new ArrayList<String>();
 
 	  /*This is how elements should be added to the array list*/
-	  obj.add("Ajeet");
-	  obj.add("Harry");
+	  obj.add(null);
+	  obj.add(null);
 	  obj.add("Chaitanya");
 	  obj.add("Steve");
 	  obj.add("Anuj");
--- https/github.com/thedeveshbajpai/Practise/src/hashMaptest/hashmaptest.java
+++ https/github.com/thedeveshbajpai/Practise/src/hashMaptest/hashmaptest.java
@@ -1,6 +1,7 @@
 package hashMaptest;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class hashmaptest {
 
@@ -10,7 +11,13 @@ public class hashmaptest {
 
 		Integer one=new Integer(1);
 		hm.put(new Double("2"),"d");
-		hm.put(one,"e");
+		hm.put(one,null);
+		hm.put(null,"dev");
+		hm.put(null,"devNew");
 		System.out.println("hashmaptest.main()"+hm);
+		Iterator itr=hm.entrySet().iterator();
+		while(itr.hasNext()){
+			System.out.print(" "+itr.next());
+		}
 	}
 }
